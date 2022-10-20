0e15702748d693b7de2a9f37cebecf2fee802632
AliZaid96 <ali.alhussein96@gmail.com>
2022-10-21 01:06:20 +0300

    Set debug to true

--- Ecommerce_Application/settings.py
+++ Ecommerce_Application/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-b&=u6_4z&(zka_7clk()myq-zzqtcw2@v2lh&c7uo-2m+=u$*j'
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://e-shop-proj.herokuapp.com/', 'www.e-shop-proj.herokuapp.com/']
 
