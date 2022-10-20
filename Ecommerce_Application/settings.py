4bf2a5ba9abaf97fc15cab53dbcedc8eecef0b2d
AliZaid96 <ali.alhussein96@gmail.com>
2022-10-21 01:01:23 +0300

    Added whitenoise compersion

--- Ecommerce_Application/settings.py
+++ Ecommerce_Application/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media_files")
